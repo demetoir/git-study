df012f58a6a52250d07438f612ca4b51baee4886
demetoir-desktop-ubuntu <wnsqlehlswk@gmail.com>
2018-01-09 09:56:23 +0900

    master

--- master.py
+++ master.py
@@ -10,3 +10,39 @@ master master
 8
 9
 0102304005
+this is master
+master master
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0102304005
+this is master
+master master
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0102304005
+this is master
+master master
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0102304005
